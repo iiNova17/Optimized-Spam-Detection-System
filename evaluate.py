d1394e6397764264d3c3a108b45614f7489f3657
iiNova17 <90964512+iiNova17@users.noreply.github.com>
2026-09-07 22:38:36 +0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -6,36 +6,37 @@ from logistic_regression import train_logistic_regression, evaluate_logistic_reg
 
 from vectorize_data import vectorize_data
 
+def evaluate_models():
+    # Load and vectorize data
+    X_train, X_test, y_train, y_test, vectorizer = vectorize_data(
+            "data/cleaned.csv"
+        )
 
-# Load and vectorize data
-X_train, X_test, y_train, y_test, vectorizer = vectorize_data(
-        "data/cleaned.csv"
-    )
-
-#SVM model
-svm_model = train_svm(X_train,y_train)
-svm_results, svm_matrix = evaluate_svm(svm_model,X_test,y_test)
-
-
-#Naive Bayes model
-naive_bayes_model = train_naive_bayes(X_train,y_train)
-naive_bayes_results, naive_bayes_matrix = evaluate_naive_bayes(naive_bayes_model,X_test,y_test)
+    #SVM model
+    svm_model = train_svm(X_train,y_train)
+    svm_results, svm_matrix = evaluate_svm(svm_model,X_test,y_test)
 
 
-#Logistic Regression model
-logistic_regression_model = train_logistic_regression(X_train,y_train)
-logistic_regression_results, logistic_regression_matrix = evaluate_logistic_regression(logistic_regression_model,X_test,y_test)
+    #Naive Bayes model
+    naive_bayes_model = train_naive_bayes(X_train,y_train)
+    naive_bayes_results, naive_bayes_matrix = evaluate_naive_bayes(naive_bayes_model,X_test,y_test)
 
 
-results = pd.DataFrame([
-    svm_results,
-    naive_bayes_results,
-    logistic_regression_results
-])
+    #Logistic Regression model
+    logistic_regression_model = train_logistic_regression(X_train,y_train)
+    logistic_regression_results, logistic_regression_matrix = evaluate_logistic_regression(logistic_regression_model,X_test,y_test)
 
 
-print("\nModels Comparison: ")
+    results = pd.DataFrame([
+        svm_results,
+        naive_bayes_results,
+        logistic_regression_results
+    ])
 
-print(results)
 
+    print("\nModels Comparison: ")
 
+    print(results)
+
+if __name__ == "__main__":
+    evaluate_models()
